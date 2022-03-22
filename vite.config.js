773b1754f7a7b917f67cdce7a2db3d313f962d18
Nate Martin <natem@element.io>
2022-03-22 13:08:31 -0700

    cleaned up vite config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,8 @@
 import { defineConfig } from "vite";
 import crossOriginIsolation from "vite-plugin-cross-origin-isolation";
-import worker, {bundleHelper} from "vite-plugin-worker";
 
 export default defineConfig({
   plugins: [
     crossOriginIsolation(),
-    bundleHelper(),
-    worker()
   ],
 });
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,8 @@
 import { defineConfig } from "vite";
 import crossOriginIsolation from "vite-plugin-cross-origin-isolation";
-import worker, {pluginHelper} from "vite-plugin-worker";
 
 export default defineConfig({
   plugins: [
     crossOriginIsolation(),
-    pluginHelper(),
-    worker({})
   ],
 });
