1baa74e01d2d04df74911c4547d4619fc755a774
Nate Martin <natem@element.io>
2022-03-24 15:19:39 -0700

    input state

--- src/GameWorker.ts
+++ src/GameWorker.ts
@@ -1,8 +1,10 @@
 import { addViewMatrix4, addViewVector3, addViewVector4 } from "./component/transform";
 import { addView, createCursorBuffer } from './allocator/CursorBuffer'
 import { maxEntities } from "./config";
-import { copyToWriteBuffer, swapWriteBuffer, TripleBufferState } from "./TripleBuffer";
+import { copyToWriteBuffer, getReadBufferIndex, swapReadBuffer, swapWriteBuffer, TripleBufferState } from "./TripleBuffer";
 import { createRemoteResourceManager, loadRemoteGLTF, RemoteResourceManager } from "./ResourceManager";
+import { createInputState, getInputButtonDown, getInputButtonHeld, InputState } from "./input/InputManager";
+import { InputArray, Input } from "./input/InputKeys";
 
 globalThis.addEventListener("message", onMessage);
 
@@ -30,6 +32,8 @@ const Transform = {
 
 const state: {
   tripleBuffer: TripleBufferState,
+  inputTripleBuffer: TripleBufferState,
+  inputStates: InputState[],
   frameRate: number,
   renderWorkerPort: MessagePort,
   then: number,
@@ -37,6 +41,8 @@ const state: {
   resourceManager: RemoteResourceManager,
 } = {
   tripleBuffer: null,
+  inputTripleBuffer: null,
+  inputStates: null,
   frameRate: null,
   renderWorkerPort: null,
   then: 0,
@@ -47,7 +53,7 @@ const state: {
 function onMessage({ data: [type, ...args] }) {
   switch (type) {
     case "init":
-      init(args[0]);
+      init(args[0], args[1]);
       break;
     case "start":
       start(args[0], args[1], args[2]);
@@ -55,8 +61,13 @@ function onMessage({ data: [type, ...args] }) {
   }
 }
 
-function init(renderWorkerPort) {
+function init(inputTripleBuffer, renderWorkerPort) {
   console.log("GameWorker initialized");
+
+  state.inputTripleBuffer = inputTripleBuffer;
+  state.inputStates = inputTripleBuffer.buffers
+    .map(buffer => createCursorBuffer(buffer))
+    .map(buffer => createInputState(InputArray, buffer));
 
   if (renderWorkerPort) {
     state.renderWorkerPort = renderWorkerPort;
@@ -71,16 +82,32 @@ const rndRange = (min, max) => {
 
 const createEntity = (eid: number) => {
   entities.push(eid);
+
   const position = Transform.position[eid];
+  const rotation = Transform.rotation[eid];
+
   position[0] = rndRange(-20, 20);
   position[1] = rndRange(-20, 20);
   position[2] = rndRange(-20, 20);
 
-  if (state.renderWorkerPort) {
-    state.renderWorkerPort.postMessage(['addEntity', eid]);
-  } else {
-    globalThis.postMessage(['addEntity', eid])
-  }
+  rotation[0] = rndRange(0,5);
+  rotation[1] = rndRange(0,5);
+  rotation[2] = rndRange(0,5);
+
+  const port = state.renderWorkerPort || globalThis;
+  port.postMessage(['addEntity', eid])
+}
+
+const createCamera = (eid: number) => {
+  entities.push(eid);
+
+  const position = Transform.position[eid];
+  position[0] = 0;
+  position[1] = 5;
+  position[2] = 40;
+
+  const port = state.renderWorkerPort || globalThis;
+  port.postMessage(['addCamera', eid])
 }
 
 function start(frameRate: number, tripleBuffer: TripleBufferState, resourceManagerBuffer: SharedArrayBuffer) {
@@ -89,7 +116,9 @@ function start(frameRate: number, tripleBuffer: TripleBufferState, resourceManag
   state.tripleBuffer = tripleBuffer;
   state.resourceManager = createRemoteResourceManager(resourceManagerBuffer);
 
-  for (let i = 0; i < maxEntities; i++) {
+  createCamera(0);
+
+  for (let i = 1; i < maxEntities; i++) {
     createEntity(i);
   }
 
@@ -98,16 +127,34 @@ function start(frameRate: number, tripleBuffer: TripleBufferState, resourceManag
   update();
 }
 
+const inputSystem = (dt) => {
+  const eid = 0;
+  const position = Transform.position[eid];
+  swapReadBuffer(state.inputTripleBuffer)
+  const readableIndex = getReadBufferIndex(state.inputTripleBuffer);
+  const inputState = state.inputStates[readableIndex];
+  if (getInputButtonHeld(inputState, Input.KeyW))
+    position[2] -= dt * 25;
+  if (getInputButtonHeld(inputState, Input.KeyS))
+    position[2] += dt * 25;
+  if (getInputButtonHeld(inputState, Input.KeyA))
+    position[0] -= dt * 25;
+  if (getInputButtonHeld(inputState, Input.KeyD))
+    position[0] += dt * 25;
+}
+
 const rotationSystem = (dt) => {
-  for (let i = 0; i < entities.length; i++) {
+  for (let i = 1; i < entities.length; i++) {
     const eid = entities[i];
     const rotation = Transform.rotation[eid];
     rotation[0] += 0.5 * dt;
     rotation[1] += 0.5 * dt;
+    rotation[2] += 0.5 * dt;
   }
 }
 
 const pipeline = (dt) => {
+  inputSystem(dt);
   rotationSystem(dt);
 }
 
